392d1cb2cdc9c66ad7ddb76f109df90b291000c3
Kira Shatulova <kirashatulova@Kiras-MacBook-Pro.local>
2020-08-19 13:35:41 +0300

    update with useState hook

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Typography from "@material-ui/core/Typography";
 import Chip from "@material-ui/core/Chip"
 import Divider from "@material-ui/core/Divider"
@@ -9,82 +9,76 @@ import StepLabel from "@material-ui/core/StepLabel";
 import StepContent from "@material-ui/core/StepContent";
 import Button from "@material-ui/core/Button";
 
-class Experience extends React.Component {
+export const Experience = (props) => {
 
-    state = {
-        activeStep: 0
+    let [activeStepState, changeActiveStep] = useState(0)
+
+    const handleNext = () => {
+        changeActiveStep(activeStepState + 1 );
     };
 
-    handleNext = () => {
-        this.setState({ activeStep: this.state.activeStep + 1 })
+    const handleBack = () => {
+        changeActiveStep(activeStepState - 1 );
     };
 
-    handleBack = () => {
-        this.setState({ activeStep: this.state.activeStep - 1 })
+    const handleReset = () => {
+        changeActiveStep(0);
     };
 
-    handleReset = () => {
-        this.setState({ activeStep: 0 })
-    };
+    const steps = props.experienceItems.map((item)=> item.role);
 
-    steps = this.props.experienceItems.map((item)=> item.role);
-
-    render() {
-
-        return (
-            <div className='content'>
-                <Typography component="h2" variant="h2" gutterBottom className={classes.title}>
-                    Experience
-                </Typography>
-                <Stepper activeStep={this.state.activeStep} orientation="vertical">
-                    {this.steps.map((label, index) => (
-                        <Step key={index}>
-                            <StepLabel>{label}</StepLabel>
-                            <StepContent>
-                                <Typography component="h4" variant="h4" gutterBottom>
-                                    {this.props.experienceItems[index].role}
-                                </Typography>
-                                <Typography component="h5" variant="h5" gutterBottom>
-                                    {this.props.experienceItems[index].company}
-                                </Typography>
-                                <Typography component="h6" variant="h6" gutterBottom color="textSecondary">
-                                    {this.props.experienceItems[index].from} – {this.props.experienceItems[index].to}
-                                </Typography>
-                                <Typography component="h6" variant="body1" gutterBottom>
-                                    {this.props.experienceItems[index].description}
-                                </Typography>
-                                <br/>
+    return (
+        <div className='content'>
+            <Typography component="h2" variant="h2" gutterBottom className={classes.title}>
+                Experience
+            </Typography>
+            <Stepper activeStep={activeStepState} orientation="vertical">
+                {steps.map((label, index) => (
+                    <Step key={index}>
+                        <StepLabel>{label}</StepLabel>
+                        <StepContent>
+                            <Typography component="h4" variant="h4" gutterBottom>
+                                {props.experienceItems[index].role}
+                            </Typography>
+                            <Typography component="h5" variant="h5" gutterBottom>
+                                {props.experienceItems[index].company}
+                            </Typography>
+                            <Typography component="h6" variant="h6" gutterBottom color="textSecondary">
+                                {props.experienceItems[index].from} – {props.experienceItems[index].to}
+                            </Typography>
+                            <Typography component="h6" variant="body1" gutterBottom>
+                                {props.experienceItems[index].description}
+                            </Typography>
+                            <br/>
+                            <div>
+                                {props.experienceItems[index].skills.map((skill, i) => (
+                                    <Chip key={i} className={classes.chip} variant="outlined" color="secondary" label={skill} />
+                                ))}
+                            </div>
+                            <br/>
+                            <Divider light />
+                            <br/>
+                            <div>
                                 <div>
-                                    {this.props.experienceItems[index].skills.map((skill, i) => (
-                                        <Chip key={i} className={classes.chip} variant="outlined" color="secondary" label={skill} />
-                                    ))}
+                                    <Button
+                                        disabled={activeStepState === 0}
+                                        onClick={handleBack}>
+                                        Back
+                                    </Button>
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={ activeStepState === props.experienceItems.length - 1 ? handleReset : handleNext}>
+                                        {activeStepState === props.experienceItems.length - 1 ? 'Finish' : 'Next'}
+                                    </Button>
                                 </div>
-                                <br/>
-                                <Divider light />
-                                <br/>
-                                <div>
-                                    <div>
-                                        <Button
-                                            disabled={this.state.activeStep === 0}
-                                            onClick={this.handleBack}>
-                                            Back
-                                        </Button>
-                                        <Button
-                                            variant="contained"
-                                            color="primary"
-                                            onClick={ this.state.activeStep === this.props.experienceItems.length - 1 ? this.handleReset : this.handleNext}>
-                                            {this.state.activeStep === this.steps.length - 1 ? 'Finish' : 'Next'}
-                                        </Button>
-                                    </div>
-                                </div>
-                            </StepContent>
-                        </Step>
-                    ))}
-                </Stepper>
-            </div>
-        );
-
-    }
+                            </div>
+                        </StepContent>
+                    </Step>
+                ))}
+            </Stepper>
+        </div>
+    );
 }
 
 export default Experience;
